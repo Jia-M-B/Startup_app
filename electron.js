7212c45861d4aa1b042ed88cb4ade3cc16701498
Jia-M-B <jiayikoh1@gmail.com>
2026-03-06 21:35:39 +0800

    first commit

---
+++ electron.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { exec } = require('child_process')
+app.disableHardwareAcceleration()
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  
+  win.loadURL('http://localhost:3000')
+
+  win.webContents.on('did-fail-load', (_event, code, desc) => {
+    console.error('Failed to load:', code, desc)
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on("lunch-mode",(_event, mode) => {
+  if(mode == "Coding"){
+    exec('explorer.exe spotify:')
+    exec('cmd /c start "" "C:\\Users\\Asus\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"')
+    exec('start steam://steam://rungameid/3548580')
+    exec('start chrome --profile-directory="Profile 1" https://claude.ai')
+    //the chrome open wihtout token
+    //stema didn't open properly 
+  }
+
+  if(mode == "Game"){
+    exec('explorer.exe spotify:')
+    exec('start discord://')
+    exec('start chrome --profile-directory="default" https://www.bilibili.com/')
+    exec('start "C:\Users\Asus\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Steam.lnk"')
+  }
+
+  if(mode == "Study"){
+    exec('start chrome --profile-directory="default" https://mytimes.taylors.edu.my/login/index.php')
+  }
+})
